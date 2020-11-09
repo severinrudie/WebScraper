4613a0780f558f488c6168e96c01e55035da289b
Severin Rudie <13170306+severinrudie@users.noreply.github.com>
2020-11-09 13:08:23 -0800

    Run KtLint on PRs to main

--- src/main/kotlin/e/severin/rudie/Hello.kt
+++ src/main/kotlin/e/severin/rudie/Hello.kt
@@ -3,4 +3,3 @@ package e.severin.rudie
 fun main(args: Array<String>) {
     println("Hello, World")
 }
-
--- src/test/kotlin/e/severin/rudie/HelloTest.kt
+++ src/test/kotlin/e/severin/rudie/HelloTest.kt
@@ -1,12 +1,10 @@
 package e.severin.rudie
 
 import org.junit.Test
-import kotlin.test.assertEquals
 
 class HelloTest {
 
     @Test
     fun tc1() {
-
     }
 }
