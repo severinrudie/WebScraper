3cc097efe40b1b96dc032180dd2f708bee9668eb
severinrudie <severinrudie@gmail.com>
2020-11-11 13:42:13 -0800

    Closes #7, closes #8: parse target out of response

--- Shared/src/main/kotlin/Contract.kt
+++ Shared/src/main/kotlin/Contract.kt
@@ -1,15 +1,21 @@
 package e.severin.rudie
 
-typealias RawResponse = String
-
 interface Contract {
 
     interface Fetcher {
-
         fun fetch(url: String): ResponseFuture
 
         interface ResponseFuture {
             fun setOnSuccess(action: (RawResponse) -> Unit)
         }
+    }
+
+    interface Parser {
+        fun parse(
+            metadata: Response.Metadata,
+            raw: RawResponse,
+            targetDefinition: Parsing.TargetMatchDefinition, // TODO list of definitions
+            linkMatchDefinition: Parsing.LinkMatchDefinition
+        ): Parsing.Result
     }
 }
---
+++ Shared/src/main/kotlin/DomainObjects.kt
@@ -0,0 +1,32 @@
+package e.severin.rudie
+
+import java.net.URL
+
+typealias RawResponse = String
+
+object Response {
+    data class Metadata(val url: URL)
+}
+
+object Parsing {
+
+    // TODO revisit this.  Need to think about what I'm trying to parse out, and also how I need to target it
+    data class TargetMatchDefinition(val withTag: String?, val withText: String?)
+
+    sealed class LinkMatchDefinition {
+        object All : LinkMatchDefinition()
+        object None : LinkMatchDefinition()
+        // TODO Note that attribute 'rel="nofollow"' on <a> is used by Google to stop spiders from crawling. See
+        // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel
+        // TODO update to respect robots.txt
+    }
+
+    data class Result(
+        val metadata: Metadata,
+        val foundText: List<String>,
+        // Follow up is a set to avoid duplication, but cannot be Set<URL> because URL#equals makes a network call
+        val followUpLinks: Set<String>
+    )
+
+    data class Metadata(val url: URL)
+}
